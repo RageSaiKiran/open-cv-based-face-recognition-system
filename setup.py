db7634bcce404212854823356b8d73f46f20ee8c
RageSaiKiran <kirannarayan2580@gmail.com>
2022-07-13 01:28:04 +0530

    adding to main file

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ if sys.platform == 'win32':
     base = None
 
 print("bhanukiran2")
+print("chandakala")
+
 executables = [Executable("train.py", base=base)]
 
 packages = ["idna","os","sys","cx_Freeze","tkinter","cv2","setup",
